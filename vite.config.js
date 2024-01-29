46719a9ff6704911b7aaf67bc313dc3676741c84
Hicham2012 <99765449+Hicham2012@users.noreply.github.com>
2024-01-29 19:58:36 +0100

    MODIFY: add pages to vite.config.js and move static folder outside of src along with modifying paths

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import glsl from 'vite-plugin-glsl'
+import { resolve } from 'path'
+// import { defineConfig } from 'vite'
 
 const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
 
 export default {
     root: 'src/',
-    publicDir: './',
+    publicDir: '../static/',
     base: './',
     server: {
         host: true,
@@ -14,7 +16,17 @@ export default {
         outDir: '../dist',
         emptyOutDir: true,
         sourcemap: true,
-        assetsInlineLimit: 204096
+        assetsInlineLimit: 204096,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                enter: resolve(__dirname, 'src/Enter_Page/enter.html'),
+                human_nature: resolve(__dirname, 'src/Human_Nature_Page/human_nature.html'),
+                weather: resolve(__dirname, 'src/Weather_Page/weather_visualisation.html'),
+                intractive_art: resolve(__dirname, 'src/Interactive_Art_Page/interactive.html'),
+                generative: resolve(__dirname, 'src/Generative_Page/generative.html')
+            },
+        },
     },
 
     plugins: [
